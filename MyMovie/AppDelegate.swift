f0fc0ec1f2cda66c16ace0c5a22cc50731eff2bd
Andrey Bashkirtcev <nruzumak@gmail.com>
2020-07-28 12:21:34 +0500

    add api key

--- MyMovie/AppDelegate.swift
+++ MyMovie/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    let apiKey = "6b09ac59"
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
